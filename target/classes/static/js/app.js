145b8822a288ca7911febf3c30204d674bb8ccbc
OkarRojas <orojasb@unal.edu.co>
2025-04-25 18:46:02 -0500

    Delete target/classes directory

--- target/classes/static/js/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let alert = document.querySelector(".alert-success");
-    if (alert) {
-        alert.classList.add("show"); // Muestra la alerta
-        setTimeout(() => {
-            alert.style.opacity = "0"; // Desvanece la alerta
-        }, 3000);
-    }
-});
